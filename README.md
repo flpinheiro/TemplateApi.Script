2a36281bbf6f367de403f67b05dd5237b16b84b2
Felipe Pinheiro <flpinheiro@gmail.com>
2023-07-24 16:19:22 -0300

    wip

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # Create new solution script
 
+This is a powershell script to build a DotNet Solution Template. To use this template execute the following command.
+
 `Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser`
 
 `./solution.ps1 -Name <Name> -Framework <FRAMEWORK>`
+
+## Default values
+
+The Default values are:
+
+* `<Name> = Template`
+* `<Framework> = net7.0`
+
+## Possible Framework Values
 
 `-Framework <FRAMEWORK>`
 
@@ -21,7 +32,40 @@ The following table lists the default values according to the SDK version number
 
 execute `dotnet --list-sdks` to know wich sdk is installed on yur machine
 
-## default values
+# Template Architeture
 
-* `-Name = Template`
-* `-Framework = net6.0`
+The Following Architeture will be created.
+
+![Mains Architeture](./images/Architeture.svg)
+
+* WebApi 
+  
+    Main part of the Project
+  
+   * Controllers
+   * Static Configuration Files
+   * Build Files
+   * 
+* CrossCutting
+
+    Responsable for configuration, security, logging and Authentication/Autorization connection with SSO service, if necessary
+
+* ApplicationCore
+
+    Responsable for the bussines logic
+
+* Infraestructure
+
+    Resposable for external connection logic such as
+    * Database logic
+    * Http client logic (external services)
+
+* Domain
+
+    Definition project. 
+
+    * Entities, DTOs
+    * Exceptions
+    * Constants
+    * Interfaces
+    * Validation
--- solution.ps1
+++ solution.ps1
@@ -1,20 +1,20 @@
-param([string] $Name="Template", [string] $Framework="net6.0")
+param([string] $Name="Template", [string] $Framework="net7.0")
 
 dotnet new sln -n "$Name"
 dotnet new webapi -n "$Name.WebApi" -o "$Name.WebApi" -f "$Framework"
 dotnet new classlib -n "$Name.Domain" -o "$Name.Domain" -f "$Framework"
-dotnet new classlib -n "$Name.Infra" -o "$Name.Infra" -f "$Framework"
+dotnet new classlib -n "$Name.Infrastructure" -o "$Name.Infrastructure" -f "$Framework"
 dotnet new classlib -n "$Name.CrossCutting" -o "$Name.CrossCutting" -f "$Framework"
 dotnet new xunit -n "$Name.UnitTest" -o "$Name.UnitTest" -f "$Framework"
-dotnet sln "$Name.sln" add "$Name.WebApi"
-dotnet sln "$Name.sln" add "$Name.Domain"
-dotnet sln "$Name.sln" add "$Name.Infra"
-dotnet sln "$Name.sln" add "$Name.CrossCutting"
-dotnet sln "$Name.sln" add "$Name.UnitTest"
+dotnet sln "$Name.sln" add "$Name.WebApi" --solution-folder "src"
+dotnet sln "$Name.sln" add "$Name.Domain" --solution-folder "src"
+dotnet sln "$Name.sln" add "$Name.Infrastructure" --solution-folder "src"
+dotnet sln "$Name.sln" add "$Name.CrossCutting" --solution-folder "src"
+dotnet sln "$Name.sln" add "$Name.UnitTest" --solution-folder "test"
 
-dotnet add "$Name.UnitTest/$Name.UnitTest.csproj" reference "$Name.CrossCutting/$Name.CrossCutting.csproj" "$Name.Domain/$Name.Domain.csproj" "$Name.Infra/$Name.Infra.csproj" "$Name.WebApi/$Name.WebApi.csproj"
-dotnet add "$Name.WebApi/$Name.WebApi.csproj"     reference "$Name.CrossCutting/$Name.CrossCutting.csproj" "$Name.Domain/$Name.Domain.csproj" "$Name.Infra/$Name.Infra.csproj"
-dotnet add "$Name.Infra/$Name.Infra.csproj"       reference "$Name.CrossCutting/$Name.CrossCutting.csproj" "$Name.Domain/$Name.Domain.csproj" 
+dotnet add "$Name.UnitTest/$Name.UnitTest.csproj" reference "$Name.CrossCutting/$Name.CrossCutting.csproj" "$Name.Domain/$Name.Domain.csproj" "$Name.Infrastructure/$Name.Infrastructure.csproj" "$Name.WebApi/$Name.WebApi.csproj"
+dotnet add "$Name.WebApi/$Name.WebApi.csproj"     reference "$Name.CrossCutting/$Name.CrossCutting.csproj" "$Name.Domain/$Name.Domain.csproj" "$Name.Infrastructure/$Name.Infrastructure.csproj"
+dotnet add "$Name.Infrastructure/$Name.Infrastructure.csproj"       reference "$Name.CrossCutting/$Name.CrossCutting.csproj" "$Name.Domain/$Name.Domain.csproj" 
 dotnet add "$Name.Domain/$Name.Domain.csproj"     reference "$Name.CrossCutting/$Name.CrossCutting.csproj" 
 
 
